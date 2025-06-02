404453559cc70a9a249353cc3188579ccb46d55a
Nikita <nikitapavlov0608@gmail.com>
2025-06-02 19:00:42 +0300

    Add 2 feature

--- main.go
+++ main.go
@@ -6,8 +6,16 @@ func main() {
 	arr := []int{1, 2, 3, 4, 5}
 	foo(arr)
 	fmt.Println(arr)
+
+	arr2 := []int{1, 2, 3, 4, 5}
+	foo2(arr2)
+	fmt.Println(arr2)
 }
 
 func foo(arr []int) {
 	arr[0] = 100
+}
+
+func foo2(arr2 []int) {
+	arr2[0] = 100
 }
